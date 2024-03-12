8a2c2dbf755da60fc93fee5156a1d8cfcc2634c9
Yongqi Liang <my@liangyongqi.top>
2024-03-12 17:44:56 +0800

    Created Android Projects

---
+++ Client/automotive/src/main/java/com/liangyongqi/autoapp/MainActivity.java
@@ -0,0 +1,21 @@
+/**
+ * @File: MainActivity.java
+ * @Author: liangyongqi
+ * @Email: 
+ * @Date: 2024/3/12
+ * @Time: 17:37
+ */
+package com.liangyongqi.autoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
